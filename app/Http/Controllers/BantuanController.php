78f001cb01200a90374e3b7274cef15b31385059
Muhammad Faza <m.faza1998@gmail.com>
2020-05-07 21:46:35 +0700

    menambahkan privilege kode unit kerja yang dapat dilihat masing-masing user

--- app/Http/Controllers/BantuanController.php
+++ app/Http/Controllers/BantuanController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class BantuanController extends Controller
@@ -19,12 +20,39 @@ class BantuanController extends Controller
      */
     public function kodeUnitKerja()
     {
-        $list_es3 = DB::table('eselon3')
-                ->select('unit_eselon3', 'kode_eselon3')
-                ->get();
-        $list_es2 = DB::table('eselon2')
-                ->select('unit_eselon2', 'kode_eselon2')
-                ->get();
+        switch (Auth::user()->role) {
+            case 'eselon2':
+                $list_es3 = DB::table('eselon3')
+                        ->select('unit_eselon3', 'kode_eselon3')
+                        ->where('kode_eselon2', Auth::user()->kode_satker)
+                        ->get();
+                $list_es2 = DB::table('eselon2')
+                        ->select('unit_eselon2', 'kode_eselon2')
+                        ->where('kode_eselon2', Auth::user()->kode_satker)
+                        ->get();
+                break;
+
+            case 'eselon3':
+                $list = DB::table('eselon3')
+                        ->leftJoin('eselon2', 'eselon2.kode_eselon2', 'eselon3.kode_eselon2')
+                        ->where('kode_eselon3', Auth::user()->kode_satker);
+                $list_es2 = $list
+                        ->select('unit_eselon2', 'eselon2.kode_eselon2')
+                        ->get();
+                $list_es3 = $list
+                        ->select('unit_eselon3', 'kode_eselon3')
+                        ->get();
+                break;
+
+            case 'admin':
+                $list_es3 = DB::table('eselon3')
+                        ->select('unit_eselon3', 'kode_eselon3')
+                        ->get();
+                $list_es2 = DB::table('eselon2')
+                        ->select('unit_eselon2', 'kode_eselon2')
+                        ->get();
+                break;
+        }
 
         return view('bantuan.kodeUnitKerja', compact('list_es2', 'list_es3'));
     }
